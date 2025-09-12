532a5f161654c9be2c0f2f7a544c79d800f9454a
vincenciusadyatma <adyatmav@gmail.com>
2025-09-12 15:47:11 +0700

    feat: create first hello world and tugas2

--- src/kuliah/tugas2/Main.kt
+++ src/kuliah/tugas2/Main.kt
@@ -1,2 +1,21 @@
 package kuliah.tugas2
 
+//fun wordCount(text:String):Int{
+//    // fungsi hitung jumlah kata dari text
+//}
+//fun isWordExist(text:String,word:String):Boolean{
+//    // fungsi mnegecek kata ada tidak dari sebuah text
+//}
+//fun eraseReadingSign(text:String):String{
+//    //hapus tanda baca "." ",","?","!"
+//}
+//fun main(args:Array<String>){
+//    val myText = "this is my first kotlin program. It was a simplified java language"
+//    var newText = eraseReadingSign(myText)
+//    println(newText)
+//
+//    val numberOfWords = wordCount(newText)
+//    println(numberOfWords)
+//
+//    println(isWordExist(newText,"java"))
+//}
